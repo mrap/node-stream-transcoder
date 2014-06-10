d1df33443c82f9c3553c4194c8a9efd1c7e7233a
Michael Rapadas <mike@mrap.me>
2014-06-10 06:29:09 -0700

    Transcodes mp4 and flv asynchronously
    This can be improved by chaining streams together.
    Once the first stream is finished encoding,
    the upload can be marked as complete on the client-side.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express          = require('express')
   , StreamBodyParser = require('stream-body-parser')
   , streamBodyParser = new StreamBodyParser(app)
   , Transcoder       = require('stream-transcoder')
+  , async            = require('async')
   , path             = require('path')
   , fs               = require('fs');
 
@@ -12,23 +13,41 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(bodyParser());
 
-streamBodyParser.process('*/*', function(stream, req, next){
-  var filename = req.body.filename;
-  if (!filename) return next();
 
-  // For demo, create a file on fs
-  var fileStream = fs.createWriteStream(filename);
+var DEFAULT_VIDEO_BITRATE = 2500 * 1000;
+var saveMediaStream = function(stream, filename, outputFileType, next, metadata) {
+  var completeFilename = path.join(__dirname, filename+'.'+outputFileType);
   var transcoder = new Transcoder(stream)
     .videoCodec('h264')
-    .format('mp4')
+    .videoBitrate(DEFAULT_VIDEO_BITRATE)
+    // .audioCodec('libfaac')
+    // .channels(2)
+    .sampleRate(44100)
+    .minSize(480, 270)
+    .maxSize(1280, 720)
+    .format(outputFileType)
     .on('finish', function(){
       next();
     })
     .on('error', function(err){
-      console.error(err);
-      next();
+      next(err);
     })
-    .stream().pipe(fileStream)
+    .writeToFile(completeFilename)
+};
+
+var FILE_TYPES = ['mp4', 'flv'];
+streamBodyParser.process('video/*', function(stream, req, next){
+  var filename = req.body.filename;
+  if (!filename) return next();
+
+  console.time('mediaStream')
+  async.each(FILE_TYPES, function(fileType, callback){
+    saveMediaStream(stream, filename, fileType, callback);
+  }, function(err){
+    if (err) console.error(err);
+    console.timeEnd('mediaStream')
+    next(err);
+  });
 });
 
 app.get('/', function(req, res){
